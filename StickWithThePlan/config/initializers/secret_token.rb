2fb251559b839ea0fceaf079123f629da85665f9
Christina Chu <chu463@usc.edu>
2012-12-24 14:39:08 -0800

    rails proj

---
+++ StickWithThePlan/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StickWithThePlan::Application.config.secret_token = '597fe510f671cee1d0faf101b36d61106f08cb56126bfe51ade703b52d79024088d2346b92449c68acfaa89c1bb304691b9f5be9f6d0a157f8c7a2ce974142b6'
