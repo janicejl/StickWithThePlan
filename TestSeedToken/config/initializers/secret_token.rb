e7af39dd2f401a51c1dae557553b3ec724314537
jjw1707 <jjw1707@gmail.com>
2013-05-25 11:00:08 -0700

    initial commit of test ruby on rails project to test seeding data and tokenized input.

---
+++ TestSeedToken/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSeedToken::Application.config.secret_token = '12aaf6f1333dcdcaec5bbdd9ef7de2a16de39ac1e8c1fcff24995679280b8f2903eb9a9df55527a02009280b3d3bac2108fddc6d2dceb700b659217a36105be9'
